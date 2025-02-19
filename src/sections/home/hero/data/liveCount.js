e31db66b864404ebdfc5a07bcfb241fa0dc41e4f
anantariskys <gamersindo555@gmail.com>
2025-02-19 14:01:34 +0700

    feat : add livecount data json

---
+++ src/sections/home/hero/data/liveCount.js
@@ -0,0 +1,24 @@
+const liveCount = [
+  {
+    icon: "mdi:briefcase",
+    count: "175.324",
+    label: "Lowongan Aktif",
+  },
+  {
+    icon: "lineicons:buildings-1",
+    count: "97.354",
+    label: "Perusahaan",
+  },
+  {
+    icon: "mdi:account-group",
+    count: "3.847.154",
+    label: "Kandidat",
+  },
+  {
+    icon: "mdi:briefcase-plus",
+    count: "7.532",
+    label: "Lowongan Baru",
+  },
+];
+
+export default liveCount;
