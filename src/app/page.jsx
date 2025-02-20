64b8dd5a98498e2fc816b7496fa23db2f668378b
anantariskys <gamersindo555@gmail.com>
2025-02-20 19:33:43 +0700

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,11 @@
+import CTA from "@/sections/home/cta";
+import FeaturedJob from "@/sections/home/featured";
 import Hero from "@/sections/home/hero";
 import How from "@/sections/home/how";
 import Popular from "@/sections/home/popular";
+import PopularCategory from "@/sections/home/popularCategory";
+import Testimonial from "@/sections/home/testimonial";
+import TopCompany from "@/sections/home/topCompany";
 
 
 export default function Home() {
@@ -9,6 +14,11 @@ export default function Home() {
   <Hero/>
   <Popular/>
   <How/>
+  <PopularCategory/>
+  <FeaturedJob/>
+  <TopCompany/>
+  <Testimonial/>
+  <CTA/>
   
   </>
   );
