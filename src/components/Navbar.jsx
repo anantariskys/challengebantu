b40984a59abdb4db403cc27c455d3665e7e9cbac
anantariskys <gamersindo555@gmail.com>
2025-02-20 13:27:57 +0700

    refactor(navbar) : add container

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const pathname = usePathname();
   return (
     <nav className="bg-[#f1f2f4] z-50 sticky top-0 px-4 py-2 md:p-4">
-      <div className="flex justify-between items-center max-w-[1320px] mx-auto">
+      <div className="flex justify-between items-center container">
         <Icon icon="material-symbols:menu" className="size-8 md:hidden" onClick={() => setNavbarIsOpen((prev) => !prev)} />
         <div className={`absolute ${navbarIsopen ? "left-0" : "-left-full"} top-full bg-[#f1f2f4] min-h-screen pt-3 px-4 transition-all duration-200 md:static md:min-h-fit md:p-0`}>
           <ul className="flex flex-col gap-3 text-[##5E6670] text-sm md:flex-row lg:gap-6">
