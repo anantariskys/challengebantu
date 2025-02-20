2c28dbc8d0f77631d35d9c87308ab910ee85b08e
anantariskys <gamersindo555@gmail.com>
2025-02-20 19:32:47 +0700

    revactor : padding

--- src/sections/home/popular/index.jsx
+++ src/sections/home/popular/index.jsx
@@ -5,8 +5,8 @@ import Card from "./components/Card";
 const Popular = () => {
   return (
     <section className="bg-white">
-      <main className="container py-24 space-y-8">
-        <h2 className="text-4xl font-medium">Lowongan Paling Populer</h2>
+      <main className="container py-10 md:py-24 space-y-4 md:space-y-8">
+        <h2 className="md:text-4xl text-xl font-medium">Lowongan Paling Populer</h2>
         <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-x-4 gap-y-8">
           {popular.map((item, index) => (
             <Card item={item} key={index} />
